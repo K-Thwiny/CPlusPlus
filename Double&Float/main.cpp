5ad17d89a660916450d1b6b7d70ede7fe48e5e27
Yahya <yahya.mused@student.mec.cuny.edu>
2018-05-02 14:17:07 -0400

    first commit

---
+++ Double&Float/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a = 1.1;
+    double b = 1.1;
+    if(a==b)
+        cout << "Programming Geeks!" << endl;
+    else
+        cout << "Programming is epic!" << endl;
+    return 0;
+}
